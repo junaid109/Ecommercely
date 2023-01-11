8640ab79beaa3216ace9814ceda08ad5424165e8
Junaid Malik <junmalik109@gmail.com>
2023-01-11 14:43:36 +0000

    initial

---
+++ Ecommercely.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommercely.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
