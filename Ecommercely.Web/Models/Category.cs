2d70d62cbef38e7fe52ae14e9339eb1b6b21610f
Junaid Malik <junmalik109@gmail.com>
2023-01-11 20:21:15 +0000

    Added category and product data types

---
+++ Ecommercely.Web/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommercely.Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+
+        
+
+
+    }
+}
---
+++ Ecommercely.Web/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Ecommercely.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+        
+    }
+}
