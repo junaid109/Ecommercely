b0a2fa7e7931583a0f3fb38c1d20a1a3de65407a
Junaid Malik <junmalik109@gmail.com>
2023-01-11 20:21:57 +0000

    Added dbcontext and migration

---
+++ Ecommercely.Web/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommercely.Web.Data;
+using Ecommercely.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommercely.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
+        }
+
+        
+
+
+    }
+}
---
+++ Ecommercely.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Ecommercely.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Ecommercely.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().ToTable("Category");
+            modelBuilder.Entity<Product>().ToTable("Product");
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EcommercelyDB;Trusted_Connection=True;");
+        }
+
+    }
+}
